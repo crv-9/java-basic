65e484403c2318efc8e2e1c7fef808718377a616
CRV <crv.vsqz.9@gmail.com>
2024-02-20 11:51:31 +0100

    refactor exercise 1b and add test class

---
+++ java-module/src/io/everyonecodes/java/t3_testing/consolidation_exercises/exercise1b/service_classes/NumberSorter.java
@@ -0,0 +1,19 @@
+package io.everyonecodes.java.t3_testing.consolidation_exercises.exercise1b.service_classes;
+
+import java.util.List;
+
+public class NumberSorter {
+    public static int returnGreatestNumber(List<Integer> numbers){
+        for(int currentItemIndex = 1; currentItemIndex < numbers.size(); currentItemIndex++){
+            int valueCurrentItem = numbers.get(currentItemIndex);
+            int previousItemIndex = currentItemIndex - 1;
+            while (previousItemIndex >= 0 && numbers.get(previousItemIndex) > valueCurrentItem){
+                numbers.set(previousItemIndex + 1, numbers.get(previousItemIndex));
+                previousItemIndex--;
+            }
+            numbers.set(previousItemIndex + 1, valueCurrentItem);
+        }
+        return numbers.getLast();
+
+    }
+}
---
+++ java-module/src/io/everyonecodes/java/t3_testing/consolidation_exercises/exercise1b/service_classes/NumberSorterTest.java
@@ -0,0 +1,33 @@
+package io.everyonecodes.java.t3_testing.consolidation_exercises.exercise1b.service_classes;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static io.everyonecodes.java.t3_testing.consolidation_exercises.exercise1b.service_classes.NumberSorter.returnGreatestNumber;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumberSorterTest {
+
+    static Stream<Arguments> parameters_gatherNumbers(){
+        return Stream.of(
+                Arguments.of(List.of(1),1),
+                Arguments.of(new ArrayList<>(List.of(1,2,3)),3),
+                Arguments.of(new ArrayList<>(List.of(-3, -2, -1)),-1),
+                Arguments.of(new ArrayList<>(List.of(1, 1, 1, 2)),2),
+                Arguments.of(new ArrayList<>(List.of(-1, 1)),1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters_gatherNumbers")
+    void findGreatest(List<Integer> input, double expected){
+        var result = returnGreatestNumber(input);
+        assertEquals(expected, result);
+
+    }
+}
