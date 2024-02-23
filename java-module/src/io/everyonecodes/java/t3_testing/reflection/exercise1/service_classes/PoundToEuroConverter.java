627517a88cda728b11727b4a843891eca35b8960
CRV <crv.vsqz.9@gmail.com>
2024-02-23 11:24:11 +0100

    Refactor code to simplify converter, no variable needed, value in return line directly

--- java-module/src/io/everyonecodes/java/t3_testing/reflection/exercise1/service_classes/PoundToEuroConverter.java
+++ java-module/src/io/everyonecodes/java/t3_testing/reflection/exercise1/service_classes/PoundToEuroConverter.java
@@ -2,8 +2,6 @@ package io.everyonecodes.java.t3_testing.reflection.exercise1.service_classes;
 
 public class PoundToEuroConverter {
     public static double convert(double pounds){
-        double euros = pounds / 0.85;
-
-        return euros;
+        return pounds / 0.85;
     }
 }
